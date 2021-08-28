1d9b55d479babfd5a28e1df9bdd06946d6295109
Lauren Vejajjiva <lp16563@hotmail.com>
2021-08-28 11:38:27 -0400

    heroku again

--- api/tags.js
+++ api/tags.js
@@ -6,8 +6,7 @@ const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use(( req, res, next) => {
 
-    console.log ("A request is being made to /tags");
-    
+
     next();
 
 })
@@ -24,14 +23,11 @@ tagsRouter.get('/',  async ( req, res ) => {
 });
 
 tagsRouter.get('/:tagName/posts', async (req, res, next) => {
-    // read the tagname from the params
     const { tagName } = req.params
  
     try {
-      // use our method to get posts by tag name from the db
    const allPosts = await getPostsByTagName(tagName)
  
-      // send out an object to the client { posts: // the posts }
       const tagedPosts = allPosts.filter(post => {
         if (post.active) {
           return true;
@@ -46,7 +42,6 @@ tagsRouter.get('/:tagName/posts', async (req, res, next) => {
         res.send ({tagedPosts})
 
     } catch ({ name, message }) {
-      // forward the name and message to the error handler
        next({ name, message })
     }
   });
